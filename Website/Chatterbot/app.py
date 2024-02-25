ef7a122708f3b25617c81b670f02b19abad2900f
m-liv <miya.livingston@icloud.com>
2024-02-25 07:52:52 -0500

    added bot stuff

--- Website/Chatterbot/app.py
+++ Website/Chatterbot/app.py
@@ -2,14 +2,14 @@
 import nltk
 import ssl
 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
 
-nltk.download()
+# nltk.download()
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -21,7 +21,7 @@ chatbot = ChatBot(
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[{
         'import_path': 'chatterbot.logic.BestMatch',
-        'default_response': "I'm sorry, but I didn't quite catch that. For more help on using TravelBot, enter 'Help'.",
+        'default_response': "I'm sorry, but I didn't quite catch that. For more help on using AtubiBot, enter 'Help'.",
         'maximum_similarity_threshold': 0.90}
     ],
     database_uri='sqlite:///database.sqlite3'
