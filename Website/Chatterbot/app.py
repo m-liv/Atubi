4d81ef436c2f09320f7390963fcf7df798c2f71e
m-liv <miya.livingston@icloud.com>
2024-02-25 08:13:20 -0500

    add

--- Website/Chatterbot/app.py
+++ Website/Chatterbot/app.py
@@ -2,14 +2,14 @@
 import nltk
 import ssl
 
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
-# nltk.download()
+nltk.download()
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
